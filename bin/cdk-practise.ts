b64f8682b1b6f45e9a5e917195a2f0ee2c6834e6
EC2 Default User <ec2-user@ip-172-31-30-64.eu-west-1.compute.internal>
2021-10-18 16:47:01 +0000

    Add context for dev and prod, add diff stack for each env

--- bin/cdk-practise.ts
+++ bin/cdk-practise.ts
@@ -1,10 +1,26 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { MyFirstStack } from '../lib/my-first-stack';
+import { MyArtifactBucketStack } from '../lib/my-first-stack';
 
 const app = new cdk.App();
-new MyFirstStack(app, 'MyFirstStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'eu-west-1' },
+
+const DEV_ENV = { 
+	account: app.node.tryGetContext('dev')['account'],
+	region: app.node.tryGetContext('dev')['region']
+}
+
+const PROD_ENV = { 
+	account: app.node.tryGetContext('prod')['account'],
+	region: app.node.tryGetContext('prod')['region']
+}
+
+new MyArtifactBucketStack(app, 'MyDevStack', false, {
+  env: DEV_ENV,
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+
+new MyArtifactBucketStack(app, 'MyProdStack', true, {
+  env: PROD_ENV,
 });
--- lib/my-first-stack.ts
+++ lib/my-first-stack.ts
@@ -1,17 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3  from '@aws-cdk/aws-s3';
 
-export class MyFirstStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class MyArtifactBucketStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, isProd: boolean, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const myBucket = new s3.Bucket(this, 'MyCDKBucket',{
-    	encryption: s3.BucketEncryption.KMS,
-	blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-	removalPolicy: cdk.RemovalPolicy.DESTROY,
-	autoDeleteObjects: true	
-    });
-
-    console.log(myBucket.bucketName);
+    let myArtifactBucket = null;
+    if(isProd)
+    {
+    	myArtifactBucket = new s3.Bucket(this, 'MyProdArtifactBucket',{
+    	    encryption: s3.BucketEncryption.KMS,
+	    blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+	    versioned: true,
+	    removalPolicy: cdk.RemovalPolicy.DESTROY,
+	    autoDeleteObjects: true	
+    	});
+    }else{
+    	myArtifactBucket = new s3.Bucket(this, 'MyDevArtifactBucket',{
+	    removalPolicy: cdk.RemovalPolicy.DESTROY,
+	    autoDeleteObjects: true	
+    	});
+    }
   }
 }
