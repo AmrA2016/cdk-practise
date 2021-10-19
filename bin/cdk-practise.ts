87aaaaf9ffc529c772afea4076c8c28595019d60
Amr Atef <amr.atef96@hotmail.com>
2021-10-19 13:04:44 +0000

    Change lib folder structure

--- bin/cdk-practise.ts
+++ bin/cdk-practise.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { MyArtifactBucketStack } from '../lib/my-first-stack';
+import { MyArtifactBucketStack } from '../lib/myFirstStack';
 
 const app = new cdk.App();
 
